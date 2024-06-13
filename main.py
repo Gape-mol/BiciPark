954e407ab36c130a60ebca14cd489211a0ad370a
Gape-mol <g.perez11@ufromail.cl>
2024-06-13 10:10:10 -0400

    Se ha comentado el gestor de archivos y comenzado con las variables para el funcionamiento

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import datetime
+from datetime import datetime
 import csv
 
+"""
 #Clase encargada de la Gestion de archivos
 class gestor_archivo():
     filename = "Cicloparqueaderos.csv"
@@ -18,10 +20,23 @@ class gestor_archivo():
             line_count = 0
             for row in csv_reader:
                 if line_count == 0:
-                    print(f'Column names are {", ".join(row)}')
+                    print(f"Los nombres de cada columna son: {", ".join(row)}")
                     line_count += 1
                 print(f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
                 line_count += 1
             print(f'Processed {line_count} lines.')
 
 gestor_archivo.guardar_datos
+"""
+
+marca = "bmx"
+modelo = "Todoterreno"
+color = "Negra con azul"
+numero = 101
+nombre = "John"
+matricula = 21111222324
+c = datetime.now()
+hora_actual = c.strftime("%H:%M:%S")
+fecha_actual = c.strftime("%d/%m/%Y")
+print(f"La bicicleta se ha recivido a las {hora_actual} el dia: {fecha_actual}")
+
