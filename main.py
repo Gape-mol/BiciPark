ff7c6e172df618fa7be092139aca2412615ddb4d
Gape-mol <g.perez11@ufromail.cl>
2024-06-13 09:45:18 -0400

    Actualizacion del main.py, con codigo para cargar y leer csv

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
+import datetime
+import csv
+
+#Clase encargada de la Gestion de archivos
 class gestor_archivo():
-    print("a")
+    filename = "Cicloparqueaderos.csv"
+    #Funcion de guardado de datos
+    @staticmethod
+    def guardar_datos(bicicletas, filename):
+        with open(filename, "w", newline='') as data:
+            csv_writer = csv.writer(data) #Crea un objeto csv writer que sera el encargado de escribir nuestro csv
+            csv_writer.writerow(["Marca", "Modelo", "Color", "Matricula", "Hora"]) #El objeto escribe en la primera fila los titulos de las columnas
+    #Funcion de carga de datos
+    @staticmethod
+    def cargar_datos(bicicletas, filename):
+        with open(filename, mode='r') as data:
+            csv_reader = csv.DictReader(data)
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    print(f'Column names are {", ".join(row)}')
+                    line_count += 1
+                print(f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
+                line_count += 1
+            print(f'Processed {line_count} lines.')
+
+gestor_archivo.guardar_datos
